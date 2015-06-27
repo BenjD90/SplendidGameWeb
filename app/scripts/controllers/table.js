40333ed4951c320968c515c402b0dbdc268bf452
Benjamin <BenjDaniel90@gmail.com>
2015-06-27 16:02:43 +0200

    End creating gameService, still need to be tested

--- app/scripts/controllers/table.js
+++ app/scripts/controllers/table.js
@@ -17,7 +17,7 @@ angular.module('splendidGameWebApp')
     function initRound() {
       $scope.table = $firebaseObject(Ref.child('tables/' + $routeParams.idTable));
       $scope.table.$loaded().then(function () {
-        gameService.initGame($scope.table);
+        gameService.initGame($scope.table, user);
         $scope.table.$save();
       });
     }
@@ -50,120 +50,25 @@ angular.module('splendidGameWebApp')
 
     //take one token
     $scope.getOneToken = function (tokenName) {
-      console.log($scope.pickedToken);
-      console.log(tokenName);
-      console.log(controlTokenPicked($scope.pickedToken, tokenName));
-      console.log('------------------');
-      if (controlTokenPicked($scope.pickedToken, tokenName) && $scope.table.game.token[tokenName].nb > 0) {
-        $scope.pickedToken[tokenName].nb++;
-        $scope.table.game.token[tokenName].nb--;
-
-      }
+      gameService.getOneToken(tokenName);
     };
 
     $scope.nextPlayer = function () {
-      $scope.table.players[$scope.userIndex].token = addTokens(angular.copy($scope.pickedToken), $scope.table.players[$scope.userIndex].token);
-      $scope.table.$save();
-      $scope.pickedToken = angular.copy(CST_PICKED_TOKEN_INITIAL);
+      gameService.nextPlayer();
     };
 
     $scope.cancelAction = function () {
-      initRound();
+      gameService.cancelAction();
     };
 
     $scope.quitTable = function () {
-      $scope.table.players.splice($scope.table.players.indexOf(user.uid), 1);
-      $scope.table.$save();
-      //TODO change main player
+      gameService.quitTable();
     };
 
     $scope.takeCard = function (card) {
-      if ($scope.userIndex != -1) {
-        if (!$scope.table.players[$scope.userIndex].cards) {
-          $scope.table.players[$scope.userIndex].cards = [];
-        }
-        if ($scope.table.players[$scope.userIndex].cards.indexOf(card) == -1) {
-          //We take the card
-          $scope.table.players[$scope.userIndex].cards.push(card);
-          //Take a yellow token too
-          $scope.getOneToken('yellow');
-        }
-      }
+      gameService.takeCard(card);
     };
 
-    function addTokens(pickedTokens, userTokens) {
-      var ret = {};
-      angular.forEach(pickedTokens, function (value, key) {
-        ret[key] = {};
-        if (userTokens != undefined && userTokens[key] != undefined) {
-          ret[key].nb = value.nb + userTokens[key].nb;
-        } else {
-          ret[key].nb = value.nb;
-        }
-      }, ret);
-      debugger;
-      return ret;
-    }
 
-
-
-    /**
-     * Check if the user can take a token or not
-     * Spec :
-     * Take 3 gem tokens of different colors.
-     * Take 2 gem tokens of the same color.
-     * This action is only possible if there are at least 4 tokens of the chosen color left when the player takes them
-     * @param pickedToken
-     * @param tokenWanted
-     * @return boolean true if one can else false
-     */
-    function controlTokenPicked(pickedToken, tokenWanted) {
-      var properties = {
-        sumAllPickedToken: 0,
-        numberOfDifferentsColors: 0,
-        maxPickedByColor: 0,
-        maxPickedByColorColor: undefined,
-        nbYellow: 0
-      };
-      angular.forEach(pickedToken, function (value, key) {
-        properties.sumAllPickedToken += value.nb;
-        if (value.nb != 0) {
-          properties.numberOfDifferentsColors++;
-        }
-        if (value.nb > properties.maxPickedByColor) {
-          properties.maxPickedByColor = value.nb;
-          properties.maxPickedByColorColor = key;
-        }
-        if (key == 'yellow' && value.nb > 0) {
-          properties.nbYellow++;
-        }
-      }, properties);
-
-      //take the first token
-      if (properties.sumAllPickedToken == 0) {
-        return true;
-      }
-
-      if (properties.nbYellow > 0) {
-        return false;
-      }
-
-      //exclud yellow
-      if (tokenWanted == 'yellow' && properties.sumAllPickedToken > 0) {
-        return false;
-      }
-
-      //3 colors max and only one by color and one doesn't pick in the same color
-      //Take 3 gem tokens of different colors.
-      if (properties.numberOfDifferentsColors < 3 && properties.maxPickedByColor < 2 && properties.maxPickedByColorColor != tokenWanted) {
-        return true;
-      }
-
-      //already picked one
-      //Take 2 gem tokens of the same color.
-      return (properties.sumAllPickedToken > 0 && properties.maxPickedByColor < 2 && properties.numberOfDifferentsColors < 2 && properties.maxPickedByColorColor == tokenWanted && $scope.table.game.token[tokenWanted].nb > 2);
-
-      return false;
-    }
   });
 
--- app/scripts/services/GameService.js
+++ app/scripts/services/GameService.js
@@ -6,7 +6,9 @@ angular.module('splendidGameWebApp').factory("gameService", function (CST_PICKED
   var ref = this;
 
   this.pickedToken;
-
+  this.table;
+  this.userIndex;
+  this.user;
 
   function getIndexOfUser(users, userId) {
     for (var i = 0; i < users.length; i++) {
@@ -17,9 +19,84 @@ angular.module('splendidGameWebApp').factory("gameService", function (CST_PICKED
     return -1;
   }
 
+  function addTokens(pickedTokens, userTokens) {
+    var ret = {};
+    angular.forEach(pickedTokens, function (value, key) {
+      ret[key] = {};
+      if (userTokens != undefined && userTokens[key] != undefined) {
+        ret[key].nb = value.nb + userTokens[key].nb;
+      } else {
+        ret[key].nb = value.nb;
+      }
+    }, ret);
+    debugger;
+    return ret;
+  }
+
+  /**
+   * Check if the user can take a token or not
+   * Spec :
+   * Take 3 gem tokens of different colors.
+   * Take 2 gem tokens of the same color.
+   * This action is only possible if there are at least 4 tokens of the chosen color left when the player takes them
+   * @param pickedToken
+   * @param tokenWanted
+   * @return boolean true if one can else false
+   */
+  function controlTokenPicked(pickedToken, tokenWanted) {
+    var properties = {
+      sumAllPickedToken: 0,
+      numberOfDifferentsColors: 0,
+      maxPickedByColor: 0,
+      maxPickedByColorColor: undefined,
+      nbYellow: 0
+    };
+    angular.forEach(pickedToken, function (value, key) {
+      properties.sumAllPickedToken += value.nb;
+      if (value.nb != 0) {
+        properties.numberOfDifferentsColors++;
+      }
+      if (value.nb > properties.maxPickedByColor) {
+        properties.maxPickedByColor = value.nb;
+        properties.maxPickedByColorColor = key;
+      }
+      if (key == 'yellow' && value.nb > 0) {
+        properties.nbYellow++;
+      }
+    }, properties);
+
+    //take the first token
+    if (properties.sumAllPickedToken == 0) {
+      return true;
+    }
+
+    if (properties.nbYellow > 0) {
+      return false;
+    }
+
+    //exclud yellow
+    if (tokenWanted == 'yellow' && properties.sumAllPickedToken > 0) {
+      return false;
+    }
+
+    //3 colors max and only one by color and one doesn't pick in the same color
+    //Take 3 gem tokens of different colors.
+    if (properties.numberOfDifferentsColors < 3 && properties.maxPickedByColor < 2 && properties.maxPickedByColorColor != tokenWanted) {
+      return true;
+    }
+
+    //already picked one
+    //Take 2 gem tokens of the same color.
+    return (properties.sumAllPickedToken > 0 && properties.maxPickedByColor < 2 && properties.numberOfDifferentsColors < 2 && properties.maxPickedByColorColor == tokenWanted && $scope.table.game.token[tokenWanted].nb > 2);
+
+    return false;
+  }
+
+
   return {
-    initGame: function (table) {
+    initGame: function (table, user) {
       ref.table = table;
+      ref.user = user;
 
       if (!ref.table.players) {
         ref.table.players = [];
@@ -46,11 +123,49 @@ angular.module('splendidGameWebApp').factory("gameService", function (CST_PICKED
       }
 
 
-
       ref.pickedToken = angular.copy(CST_PICKED_TOKEN_INITIAL);
 
-
       return;
+    },
+    getOneToken: function (tokenName) {
+      console.log(ref.pickedToken);
+      console.log(tokenName);
+      console.log(controlTokenPicked(ref.pickedToken, tokenName));
+      console.log('------------------');
+      if (controlTokenPicked(ref.pickedToken, tokenName) && ref.table.game.token[tokenName].nb > 0) {
+        ref.pickedToken[tokenName].nb++;
+        ref.table.game.token[tokenName].nb--;
+
+      }
+    },
+
+    nextPlayer: function () {
+      ref.table.players[ref.userIndex].token = addTokens(angular.copy(ref.pickedToken), ref.table.players[ref.userIndex].token);
+      ref.table.$save();
+      ref.pickedToken = angular.copy(CST_PICKED_TOKEN_INITIAL);
+    },
+
+    cancelAction: function () {
+      this.initGame();
+    },
+    quitTable: function () {
+      ref.table.players.splice(ref.table.players.indexOf(ref.user.uid), 1);
+      ref.table.$save();
+      //TODO change main player
+    },
+    takeCard: function (card) {
+      if (ref.userIndex != -1) {
+        if (!ref.table.players[ref.userIndex].cards) {
+          ref.table.players[ref.userIndex].cards = [];
+        }
+        if (ref.table.players[ref.userIndex].cards.indexOf(card) == -1) {
+          //We take the card
+          ref.table.players[ref.userIndex].cards.push(card);
+          //Take a yellow token too
+          ref.getOneToken('yellow');
+        }
+      }
     }
-  }
-});
+  };
+})
+;
